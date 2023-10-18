b97cbcff23131a08605bdd952a8913319064833a
TOGoS <togos00@gmail.com>
2023-10-17 19:52:04 -0500

    Add main(), bump to v0.0.6
    And add instructions for running

---
+++ src/main/java/net/nuke24/tscript34/p0012/app02/App02.java
@@ -0,0 +1,10 @@
+package net.nuke24.tscript34.p0012.app02;
+
+import net.nuke24.tscript34.p0012.lib01.GreetingGenerator;
+
+public class App02 {
+	public static void main(String[] args) {
+		// Say hi to https://coppermind.net/wiki/Hammond
+		System.err.println(new GreetingGenerator().greetingFor("Ham"));
+	}
+}
